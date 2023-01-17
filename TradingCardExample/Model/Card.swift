a9f699bc7f7e61a7c428482d9cabfff25cef0382
Leon Gell <lgell@lcs.on.ca>
2023-01-17 10:20:27 -0500

    Added  a stored property to the card layout view and replaced static text.

--- TradingCardExample/Model/Card.swift
+++ TradingCardExample/Model/Card.swift
@@ -19,5 +19,5 @@ struct Card {
 }
 
 let mrSunglasses = Card(emoji: "😎", description: "Having a sunny disposition, Mr. Sunglasses likes to cruise the highways of California in his vintage convertible.", age: "27", height: "180 cm", sign: "Pisces", color: Color("Peach"), name: "Mr. Sunglasses")
-let cryingPerson = Card(emoji: "😭", description: "lebron", age: "50", height: "160 cm", sign: "james", color: Color("lime"), name: "crying")
+let cryingPerson = Card(emoji: "😭", description: "lebron", age: "50", height: "160 cm", sign: "james", color: Color("Lime"), name: "crying")
 let happyPerson = Card(emoji: "😁", description: "happy", age: "50", height: "190 cm", sign: "lebron", color: Color("Sky"), name: "KAwHI")
